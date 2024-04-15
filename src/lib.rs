b08bea3998dcc8b3966c4ce682998f4c72785e60
rust_lang_lover <rust_lang_lover@proton.me>
2024-04-15 16:41:51 +0300

    // Some fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+mod structs;
+
 use wasm_bindgen::prelude::*;
+use crate::structs::ObjectDefinition;
 
 #[wasm_bindgen]
-pub fn process_filtration() -> () {
+pub fn process_filtration(to_filter : Vec<ObjectDefinition>, filter_type : String, filter_query : String) -> Vec<ObjectDefinition> {
+    if filter_type == "ID" {
+        to_filter.into_iter().filter(|object| object.id.to_lowercase().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+    }
+    else if filter_type == "Статус" {
+        to_filter.into_iter().filter(|object| object.request_status.to_lowercase().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+    }
+    else if filter_type == "Имя" {
 
+    }
 }
---
+++ src/structs.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+pub struct ObjectDefinition {
+    pub id : String,
+    pub request_status : String,
+    pub customer_name : String,
+    pub customer_email : String,
+    pub customer_self_description : String,
+    pub date_time_added : String
+}
