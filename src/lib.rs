95e42a967a20dcd244300368eab9cc030516ffac
rust_lang_lover <rust_lang_lover@proton.me>
2024-04-17 14:17:20 +0300

    // implemented filtration for JS project in WASM

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,85 @@
+use std::fmt::format;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use log::log;
 
-#[wasm_bindgen]
+// We need to add Deserialize and Serialize, so we could convert data between languages.
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ObjectDefinition {
-    id : u16,
-    request_status : String,
-    customer_name : String,
-    customer_email : String,
-    customer_self_description : String,
-    date_time_added : String
+    pub id : u16,
+    pub request_status : String,
+    pub customer_name : String,
+    pub customer_email : String,
+    pub customer_self_description : String,
+    pub date_time_added : String,
+    pub notes : Vec<NoteObject>
 }
-#[wasm_bindgen]
-impl ObjectDefinition {
-    #[wasm_bindgen(getter)]
-    pub fn get_id(&self) -> String {self.id.to_string().clone()}
-
-    #[wasm_bindgen(getter)]
-    pub fn get_request_status(&self) -> String {self.request_status.clone().to_lowercase()}
-
-    #[wasm_bindgen(getter)]
-    pub fn get_customer_name(&self) -> String {self.customer_name.clone().to_lowercase()}
-
-    #[wasm_bindgen(getter)]
-    pub fn get_customer_email(&self) -> String {self.customer_email.clone().to_lowercase()}
-
-    #[wasm_bindgen(getter)]
-    pub fn get_customer_description(&self) -> String {self.customer_self_description.clone().to_lowercase()}
-
-    #[wasm_bindgen(getter)]
-    pub fn get_date_time(&self) -> String {self.date_time_added.clone()}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NoteObject {
+    pub id : u16,
+    pub text_info : String,
+    pub date_time : String
 }
-
+// To pass a struct as argument from JS we stringify it and pass as &str. When we receive it here, we use serde_json::from_str to convert it to Rust's struct.
 #[wasm_bindgen]
-pub fn process_filtration(to_filter : Vec<ObjectDefinition>, filter_type : String, filter_query : String) -> Vec<ObjectDefinition> {
+pub fn process_filtration(filter_action : &str, filter_type : String, filter_query : String) -> String {
+    let to_filter : Vec<ObjectDefinition> = serde_json::from_str(filter_action).unwrap();
     if filter_type == "ID" {
-        to_filter.into_iter().filter(|object| object.get_id().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+        serde_json::to_string(
+            &to_filter
+                .into_iter()
+                .filter(|object| object.id.to_string().contains(&filter_query.to_lowercase()))
+                .collect::<Vec<ObjectDefinition>>()
+        ).unwrap()
     }
     else if filter_type == "Статус" {
-        to_filter.into_iter().filter(|object| object.get_request_status().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+        serde_json::to_string(
+            &to_filter
+                .into_iter()
+                .filter(|object| object.request_status.to_lowercase().contains(&filter_query.to_lowercase()))
+                .collect::<Vec<ObjectDefinition>>()
+        ).unwrap()
     }
     else if filter_type == "Имя" {
-        to_filter.into_iter().filter(|object| object.get_customer_name().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+        serde_json::to_string(
+            &to_filter
+                .into_iter()
+                .filter(|object| object.customer_name.to_lowercase().contains(&filter_query.to_lowercase()))
+                .collect::<Vec<ObjectDefinition>>()
+        ).unwrap()
     }
     else if filter_type == "Почта" {
-        to_filter.into_iter().filter(|object| object.get_customer_email().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+        serde_json::to_string(
+            &to_filter
+                .into_iter()
+                .filter(|object| object.customer_email.to_lowercase().contains(&filter_query.to_lowercase()))
+                .collect::<Vec<ObjectDefinition>>()
+        ).unwrap()
     }
     else {
-        to_filter.into_iter().filter(|object| object.get_customer_description().contains(&filter_query.to_lowercase())).collect::<Vec<ObjectDefinition>>()
+        serde_json::to_string(
+            &to_filter
+                .into_iter()
+                .filter(|object| object.customer_self_description.to_lowercase().contains(&filter_query.to_lowercase()))
+                .collect::<Vec<ObjectDefinition>>()
+        ).unwrap()
     }
 }
+
+// #[wasm_bindgen]
+// pub fn process_filtration2(filter_action : &str, filter_type : String, filter_query : String) -> String {
+//     wasm_logger::init(wasm_logger::Config::default());
+//     match serde_json::from_str::<Vec<ObjectDefinition>>(&filter_action) {
+//         Ok(value) => {
+//             log::info!("{}", format!("{:#?}", value));
+//             match serde_json::to_string(&value) {
+//                 Ok(_) => {}
+//                 Err(_) => {}
+//             }
+//         }
+//         Err(e) => {
+//             log::info!("{}", format!("{}", e))
+//         }
+//     }
+// }
+
